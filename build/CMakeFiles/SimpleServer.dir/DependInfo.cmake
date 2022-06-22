7387ab822203dc5640974876c8666e136d342998
januaryzeng <15841331413@163.com>
2022-06-22 20:50:13 +0800

    cmake

---
+++ build/CMakeFiles/SimpleServer.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/zxj/project/job_related/SimpleServer/src/connection_pool/connection_pool.cpp" "/home/zxj/project/job_related/SimpleServer/build/CMakeFiles/SimpleServer.dir/src/connection_pool/connection_pool.cpp.o"
+  "/home/zxj/project/job_related/SimpleServer/src/http/http_conn.cpp" "/home/zxj/project/job_related/SimpleServer/build/CMakeFiles/SimpleServer.dir/src/http/http_conn.cpp.o"
+  "/home/zxj/project/job_related/SimpleServer/src/log/logger.cpp" "/home/zxj/project/job_related/SimpleServer/build/CMakeFiles/SimpleServer.dir/src/log/logger.cpp.o"
+  "/home/zxj/project/job_related/SimpleServer/src/log/logstream.cpp" "/home/zxj/project/job_related/SimpleServer/build/CMakeFiles/SimpleServer.dir/src/log/logstream.cpp.o"
+  "/home/zxj/project/job_related/SimpleServer/src/main.cpp" "/home/zxj/project/job_related/SimpleServer/build/CMakeFiles/SimpleServer.dir/src/main.cpp.o"
+  "/home/zxj/project/job_related/SimpleServer/src/timer/timer.cpp" "/home/zxj/project/job_related/SimpleServer/build/CMakeFiles/SimpleServer.dir/src/timer/timer.cpp.o"
+  "/home/zxj/project/job_related/SimpleServer/src/util/config.cpp" "/home/zxj/project/job_related/SimpleServer/build/CMakeFiles/SimpleServer.dir/src/util/config.cpp.o"
+  "/home/zxj/project/job_related/SimpleServer/src/util/util.cpp" "/home/zxj/project/job_related/SimpleServer/build/CMakeFiles/SimpleServer.dir/src/util/util.cpp.o"
+  "/home/zxj/project/job_related/SimpleServer/src/webserver.cpp" "/home/zxj/project/job_related/SimpleServer/build/CMakeFiles/SimpleServer.dir/src/webserver.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../include/http"
+  "../include/connection_pool"
+  "../include/lock"
+  "../include/log"
+  "../include/threadpool"
+  "../include/timer"
+  "../include/util"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
